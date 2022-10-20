71d54a568492a82ab30c00f530c9e095c6523e1a
Olaf Sozański <olaf@sozanski.hub.pl>
2022-10-20 07:48:13 +0200

    Configured Bootstrap with minor SCSS variable overrides

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
