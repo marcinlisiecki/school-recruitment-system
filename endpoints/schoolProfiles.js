7871a9a6c3e46a39249c563cbbaf247b148de055
marcinlisiecki <marcinlisiecki9@gmail.com>
2022-11-14 20:01:31 +0100

    Added admin/school-profiles/delete

--- endpoints/schoolProfiles.js
+++ endpoints/schoolProfiles.js
@@ -7,20 +7,47 @@ schoolProfilesRouter.get("/:id", async (req, res) => {
   const schoolProfile = await SchoolProfile.findById(req.params.id);
   const school = await School.findOne({ profiles: schoolProfile })
 
+  if (!schoolProfile) {
+    // TODO: handle 404
+  }
+
   res.render("admin/school-profiles/view", { schoolProfile, school });
 })
 
 schoolProfilesRouter.get("/:id/edit", async (req, res) => {
   const schoolProfile = await SchoolProfile.findById(req.params.id);
+
+  if (!schoolProfile) {
+    // TODO: handle 404
+  }
+
   res.render("admin/school-profiles/edit", { schoolProfile });
 })
 
 schoolProfilesRouter.post("/:id/edit", async (req, res) => {
+  const schoolProfile = await SchoolProfile.findById(req.params.id);
   const { name, description, availablePlaces } = req.body;
 
-  await SchoolProfile.findByIdAndUpdate(req.params.id, { $set: { name, description, availablePlaces }});
+  if (!schoolProfile) {
+    // TODO: handle 404
+  }
+
+  await schoolProfile.update({ $set: { name, description, availablePlaces }});
 
   res.redirect('/admin/school-profiles/' + req.params.id);
+})
+
+schoolProfilesRouter.post("/:id/delete", async (req, res) => {
+  const schoolProfile = await SchoolProfile.findById(req.params.id);
+  const school = await School.findOne({ profiles: schoolProfile })
+
+  if (!schoolProfile) {
+    // TODO: handle 404
+  }
+
+  await schoolProfile.delete();
+
+  res.redirect('/admin/schools/' + school._id);
 })
 
 module.exports = { schoolProfilesRouter };
