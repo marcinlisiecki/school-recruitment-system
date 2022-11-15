f4679f79fa4c7685b4ba50a7c3f0b7ba71dcf2f5
KWgames3 <kuba.wolowiec03@wp.pl>
2022-11-15 21:39:55 +0100

    add searching schools

--- endpoints/publicSchools.js
+++ endpoints/publicSchools.js
@@ -3,13 +3,25 @@ const { School } = require('../models/school');
 const publicSchoolsRouter = express.Router();
 
 publicSchoolsRouter.get('/', async (req, res) => {
-    const schools = await School.find({});
-    res.render('schools/index', { schools });
+    let schools;
+    const { search } = req.query;
+    if (!search) {
+        schools = await School.find({});
+    } else {
+        schools = await School.find({ 
+            $or: [
+                {name: {'$regex': search, '$options': 'i'}}, 
+                {address: {'$regex': search, '$options': 'i'}},
+                {city: {'$regex': search, '$options': 'i'}},
+            ] 
+        });
+    }
+    res.render('schools/index', { schools, search });
 });
 
 publicSchoolsRouter.get('/:id', async (req, res) => {
     const school = await School.findById(req.params.id);
-    
+
     if (!school) {
         // TODO: handle 404
     }
